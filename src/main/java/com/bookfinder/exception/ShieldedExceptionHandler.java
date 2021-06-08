626c374fb14359b6924a022a2919d1907ab5c57a
unknown <ashzelazny@gmail.com>
2021-06-08 10:35:34 -0500

    Exception handler

---
+++ src/main/java/com/bookfinder/exception/ShieldedExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.bookfinderclient.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@ControllerAdvice
+@Slf4j
+public class ShieldedExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<String> handleRunTimeException(RuntimeException e) {
+        return error(INTERNAL_SERVER_ERROR, e);
+    }
+
+    private ResponseEntity<String> error(HttpStatus status, Exception e) {
+        log.error("Exception : ", e);
+        return ResponseEntity.status(status).body(e.getMessage());
+    }
+}
